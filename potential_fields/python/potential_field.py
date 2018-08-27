3b02c50a7e846dfb514de221f829fdcb1dcad647
lfelipev <felipemmha@ic.ufal.br>
2018-08-27 19:13:49 -0300

    mesh solved

--- potential_fields/python/potential_field.py
+++ potential_fields/python/potential_field.py
@@ -14,7 +14,9 @@ class Map():
         self.Q_star = Q_star
         self.n = n
         self.U_att = np.zeros((self.imgShape[0], self.imgShape[1]))
-    
+        self.U_rep = self.U_att
+        self.map = cv2.imread('mapa.png', 0)
+
     def attractivePotential(self):
         for x in range(0, self.imgShape[0]):
             for y in range(0, self.imgShape[1]):
@@ -27,16 +29,79 @@ class Map():
 
         fig = plt.figure()
         ax = fig.gca(projection='3d')
-        w = np.arange(1, self.imgShape[0], 0.1)
-        h = np.arange(1, self.imgShape[1], 0.1)
+        w = np.arange(0, self.imgShape[0], 1)
+        h = np.arange(0, self.imgShape[1], 1)
         X, Y = np.meshgrid(w, h)
-        print(np.shape(self.U_att))
-        #Z = np.reshape(self.U_att, np.shape(X))
-        #surf = ax.plot_surface(X, Y, Z)
 
-        #surf = ax.plot_surface(X, Y, self.U_att, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-        #fig.colorbar(surf, shrink=0.5, aspect=5)
+        surf = ax.plot_surface(X, Y, np.transpose(self.U_att))
         plt.show()
+    
+    def callMesh(self):
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        w = np.arange(0, self.imgShape[0], 1)
+        h = np.arange(0, self.imgShape[1], 1)
+        X, Y = np.meshgrid(w, h)
+
+        surf = ax.plot_surface(X, Y, np.transpose(self.U_att))
+        plt.show()
+
+    def safeDivide(self, x, y):
+        if y == 0:
+            return 0
+        return x / y
+    
+    def repulsivePotential(self):
+        height = self.imgShape[1]
+        width = self.imgShape[0]
+        for y in range(0, height):
+            for x in range(0, width):
+                d = self.Q_star
+
+                range_x = [x - (self.Q_star/2), x + (self.Q_star/2)]
+                range_y = [y - (self.Q_star/2), y + (self.Q_star/2)]
+
+                if(range_y[0] <= 0):
+                    range_y[0] = 1
+                if(range_x[0] <= 0):
+                    range_x[0] = 1
+                if(range_y[1] > height):
+                    range_y[1] = height
+                if(range_x[1] > width):
+                    range_x[1] = width
+
+                for y0 in range(int(range_y[0]), int(range_y[1])):
+                    for x0 in range(int(range_x[0]), int(range_x[1])):
+                        #print(np.shape(self.map))
+                        if(self.map[x0, y0] < 255):
+                            d_i = np.sqrt((x-x0)**2 + (y-y0)**2)
+                        
+                            if(d_i < d):
+                                d = d_i
+                                
+                
+                
+                self.U_rep[y, x] = 0.5 * self.n * (self.safeDivide(1, d) - (1/self.Q_star))**2
+               
+                sat = 50
+
+                if(self.U_rep[y, x] > sat):
+                    self.U_rep[y, x] = sat
+
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        w = np.arange(0, self.imgShape[0], 1)
+        h = np.arange(0, self.imgShape[1], 1)
+        X, Y = np.meshgrid(w, h)
+
+        surf = ax.plot_surface(X, Y, np.transpose(self.U_rep))
+        plt.show()        
+
+
+
+
+                
+                
         
 
     def loadImg(self):
@@ -71,14 +136,14 @@ def plotField():
     surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
                             linewidth=0, antialiased=False)
     fig.colorbar(surf, shrink=0.5, aspect=5)
-
     plt.show()
 
 
 def main():
     q_goal = [65, 41]
     map = Map(q_goal)
-    map.attractivePotential()
+    #map.attractivePotential()
+    map.repulsivePotential()
 
 if __name__ == '__main__':
     main()
